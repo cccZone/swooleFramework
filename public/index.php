b74599dcfdc74ae5e09e6357118909da54ac6b3f
无忧亦无怖 <285753421@qq.com>
2017-08-07 18:12:12 +0800

    index

--- public/index.php
+++ public/index.php
@@ -4,10 +4,15 @@ include '../vendor/autoload.php';
 include '../src/Kernel/Core.php';
 $app = new \Kernel\Core([ realpath('../src')], [realpath('../conf')]);
 //var_dump($app->get('config'));
-/*$app->serverStart($app->get('tcp'), function () use($app){
-        var_dump($app);
-});*/
+$app->serverStart($app->get('http'), function () use($app){
+
+});
+
+/**
+ * 爬虫示例
 $app->getContainer()->alias(Kernel\Core\DB\DB::class,Kernel\Core\DB\Mongodb::class);
 $app->doCrawler(function (\Library\Crawler\Crawler $crawler){
+        $crawler->initUrls(['']);
         $crawler->run();
 });
+*/
